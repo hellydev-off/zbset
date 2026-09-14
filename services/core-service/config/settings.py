9d8092a62effa237e184f76f449786b227bdf6b1
Helly123123 <maksim.birykov.2007@mail.ru>
2026-09-15 00:48:39 +0300

    fix: расширил дефолт ALLOWED_HOSTS в django settings

--- services/core-service/config/settings.py
+++ services/core-service/config/settings.py
@@ -9,7 +9,7 @@ JWT_SECRET_KEY = env.str("JWT_SECRET_KEY")
 
 SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 INSTALLED_APPS = [
     "django.contrib.admin",
